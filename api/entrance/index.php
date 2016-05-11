b558b4a71cdb377c1c9a62cb70069fd8400163a7
iodine <x@xiongdianpku.com>
2016-05-11 13:32:00 +0800

    Init some base files

---
+++ api/entrance/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: i2
+ * Date: 16/1/21
+ * Time: 下午9:27
+ */
+require_once "../request_and_respond/DSDRequestHandler.php";
+require_once "../config/config.php";
+
+DSDRequestHandler::handleRequest();
+?>
