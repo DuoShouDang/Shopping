bd58e7be9bc2069abc1fecadb463ccb1c6740246
jasminexie <ameliasgoldfish@gmail.com>
2016-05-20 10:28:01 +0800

    fix: authentication intercept service bug fix

--- src/app/core/core.config.js
+++ src/app/core/core.config.js
@@ -7,17 +7,4 @@
     appVersion: '0.1.0'
   });
 
-  // Setting HTML5 Location Mode
-  angular
-    .module('app.core')
-    .config(bootstrapConfig);
-
-  bootstrapConfig.$inject = ['$locationProvider', '$httpProvider'];
-
-  function bootstrapConfig($locationProvider, $httpProvider) {
-    $locationProvider.html5Mode(true).hashPrefix('!');
-    // 权限的interceptor
-    $httpProvider.interceptors.push('authInterceptor');
-  }
-
 });
