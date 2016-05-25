8eb46944af7fd0201c7ebb428409f0a115f23874
iodine <x@xiongdianpku.com>
2016-05-25 21:13:31 +0800

    shopping cart

--- _api/request_and_respond/DSDRequestShoppingcartHandler.php
+++ _api/request_and_respond/DSDRequestShoppingcartHandler.php
@@ -15,21 +15,61 @@ class DSDRequestShoppingcartHandler{
         DSDRequestResponder::respond(
             DSDDatabaseConnector::insert(
                 "shopping_cart",
-                array_merge(Utils::filter($GLOBALS["data"], ["good_id!", "number!"])), array(
+                array_merge(Utils::filter($GLOBALS["data"], ["good_id!", "number!", "sort_identifier!"])), array(
                 "user_id"=>DSDAuthorizationChecker::getCurrentUid()
             ))
         );
     }
-    public static function modify($good_id){
-        DSDRequestResponder::respond(
-            DSDDatabaseConnector::update(
-                "shopping_cart",
-                Utils::filter($GLOBALS["data"], ["number!"]),
-                "user_id=:uid AND good_id=:gid",
-                array(
-                    ":uid"=>DSDAuthorizationChecker::getCurrentUid(),
-                    ":gid"=>$good_id
+    public static function modify($good_id, $sid){
+        if($_SERVER["REQUEST_METHOD"]=="PUT"){
+            DSDRequestResponder::respond(
+                DSDDatabaseConnector::update(
+                    "shopping_cart",
+                    Utils::filter($GLOBALS["data"], ["number!"]),
+                    "user_id=:uid AND good_id=:gid AND sort_identifier=:sid",
+                    array(
+                        ":uid"=>DSDAuthorizationChecker::getCurrentUid(),
+                        ":gid"=>$good_id,
+                        ":sid"=>$sid
+                    )
                 )
+            );
+        }elseif($_SERVER["REQUEST_METHOD"]=="POST"){
+            DSDRequestResponder::respond(
+                DSDDatabaseConnector::insert(
+                    "shopping_cart",
+                    array_merge(Utils::filter($GLOBALS["data"], ["good_id!", "number!", "sort_identifier!"])), array(
+                    "user_id"=>DSDAuthorizationChecker::getCurrentUid()
+                ))
+            );
+        }elseif($_SERVER["REQUEST_METHOD"]=="DELETE"){
+            DSDRequestResponder::respond(
+                DSDDatabaseConnector::write("delete from shopping_cart WHERE user_id=:uid AND good_id=:gid AND sort_identifier=:sid", array(
+                    ":uid"=>DSDAuthorizationChecker::getCurrentUid(),
+                    ":gid"=>$good_id,
+                    ":sid"=>$sid
+                ))
+            );
+        }
+    }
+    public static function delete($good_id, $sid){
+        DSDRequestResponder::respond(
+            DSDDatabaseConnector::write("delete from shopping_cart WHERE user_id=:uid AND good_id=:gid AND sort_identifier=:sid", array(
+                ":uid"=>DSDAuthorizationChecker::getCurrentUid(),
+                ":gid"=>$good_id,
+                ":sid"=>$sid
+            ))
+        );
+    }
+    public static function get(){
+        DSDRequestResponder::respond(true, null,
+            array_map(function($one){
+                $info=DSDDatabaseConnector::get_first_match("select name, info from goods WHERE gid=:gid", array(":gid"=>$one["good_id"]));
+                DSDGoodsManager::restore_info($info);
+                $one["product_info"]=array_merge($info["info"][$one["sort_identifier"]], array("product_name"=>$one["name"]));
+                return $one;
+            }, DSDDatabaseConnector::read("select * from shopping_cart WHERE user_id=:uid",
+                array(":uid"=>DSDAuthorizationChecker::getCurrentUid()))
             )
         );
     }
